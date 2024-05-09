935d5104f8511150d93a0cb96790c01f5ab7ddde
Aboood <abdu3793@stud.kea.dk>
2024-05-09 10:28:27 +0200

    "..."

--- components/Content.jsx
+++ components/Content.jsx
@@ -10,7 +10,7 @@ export default function Content() {
           <a href="https://www.linkedin.com/in/abdulhamid-alsaati-844572204/" target="_blank">
             Link To LinkedIn
           </a>
-          <a href="https://drive.google.com/file/d/1EnQmQ7wPzzjnc41SdM6qqZA1gzEsYq7B/view?usp=sharing" target="_blank">
+          <a href="https://drive.google.com/file/d/1v0T2roMxquPo0jSwtZZLyk1v_Fmfc5vy/view?usp=sharing" target="_blank">
             Link To CV
           </a>
         </div>
